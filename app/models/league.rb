7decc059e5bca63680653338b6339632194bb3e9
Nat Webb <natrwebb@gmail.com>
2014-06-12 11:09:11 -0500

    league owner is now commissioner

--- app/models/league.rb
+++ app/models/league.rb
@@ -1,5 +1,6 @@
 class League < ActiveRecord::Base
   belongs_to :user
+  belongs_to :commissioner, :class_name => :User, :foreign_key => 'user_id'
   has_many :teams
   has_many :seasons
 
--- spec/models/league_spec.rb
+++ spec/models/league_spec.rb
@@ -1,5 +1,6 @@
 describe League do
   it { should belong_to :user }
+  it { should belong_to :commissioner }
   it { should have_many :teams }
   it { should have_many :seasons }
   it { should validate_presence_of :user }
