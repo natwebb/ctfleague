81ef8b3ffd8afb0244a38b91b701affa31b17805
Nat Webb <natrwebb@gmail.com>
2014-06-15 12:43:22 -0500

    users now have memberships on show page

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   has_many :leagues
+  has_many :memberships
+  #has_many :leagues, through: :memberships
 
   validates :username, uniqueness: true, format: { with: /\A[a-zA-Z0-9]+\z/, message: "username can only contain letters and numbers" }
   validates_presence_of :email
